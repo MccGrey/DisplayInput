0056167b00fbcfdce09c0bd8667863b50a0f0095
MccGrey <chuxzzie01@yahoo.com>
2023-01-07 04:29:22 +0100

    button component

--- src/components/Users/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Button.module.css";
-
-const Button = (props) => {
-  return <div className={classes.Button}>{props.children}</div>;
-};
-
-export default Button;
