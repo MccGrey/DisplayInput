2ccb25dc71d900bf7667a88d6e501a0c2597a1ed
MccGrey <chuxzzie01@yahoo.com>
2023-01-08 03:44:49 +0100

    function to remove error message

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const AddUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "invalid input",
+        message: "please enter a valid name and age (non-emepty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "invalid age",
+        message: "please enter a valid age (> 0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -28,9 +37,19 @@ const AddUser = (props) => {
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
+
+  const errorHandler = () => {
+    setError(null);
+  };
   return (
     <>
-      <ErrorModal title="An error occured!" message="something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={AddUserHandler}>
           <label htmlFor="username">Username</label>
